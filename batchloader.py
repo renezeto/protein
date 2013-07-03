64ea120aff799c068188c6e09a64a1f88da7ff31
rene <rene@ubuntu.(none)>
2013-07-03 15:42:45 -0700

    batch loader

--- batchloader.py
+++ batchloader.py
@@ -1,4 +1,16 @@
 from __future__ import division
 import numpy as np
 import sys
+import os
 
+batch_pill = []
+i=1
+while i<20:
+    a = abs(i/2)
+    b = abs(5-i/2)
+    if ([a, b] not in batch_pill) and ([b, a] not in batch_pill):
+        batch_pill+=[[a,b]]
+    i+=1
+
+for job in batch_pill:
+    os.system("srun ./protein_microscopy p "+str(job[0])+" "+str(job[1])+" 0.00 0.00 15.00 > /dev/null")
