6dbd69180ec998c744c850806c5b5f193342d2a1
rene <rene@ubuntu.(none)>
2013-07-03 18:13:11 -0700

    .

--- batchloader.py
+++ batchloader.py
@@ -5,21 +5,32 @@ import os
 import subprocess
 import time
 
-batch_pill = []
+#set up simulations to be run:
+batch_pill_simulations = []
 i=1
 while i<20:
     a = abs(i/2)
     b = abs(5-i/2)
-    if ([a, b] not in batch_pill) and ([b, a] not in batch_pill):
-        batch_pill+=[[a,b]]
+    if ([a, b] not in batch_pill_simulations) and ([b, a] not in batch_pill_simulations):
+        batch_pill_simulations+=[[a,b]]
     i+=1
 
-processes = set()
-max_processes = 7
+#simulation subprocess management:
+if '-sim' in sys.argv:
+    processes = set()
+    max_processes = 7
+    for job in batch_pill_simulations:
+        processes.add(subprocess.Popen(['./protein_microscopy','p',str(job[0]),str(job[1]),'0.00','0.00','15.00']))
+        if len(processes) >= max_processes:
+            os.wait()
+            processes.difference_update(p for p in processes if p.poll() is not None)
 
-for job in batch_pill:
-    processes.add(subprocess.Popen(['./protein_microscopy','p',str(job[0]),str(job[1]),'0.00','0.00','15.00']))
-    if len(processes) >= max_processes:
-        os.wait()
-        processes.difference_update(
-            p for p in processes if p.poll() is not None)
+if '-plot' in sys.argv:
+    processes = set()
+    max_processes = 14
+    for job in batch_pill_simulations:
+        processes.add(subprocess.Popen(['python','pyplots/extrema.py','p',str(job[0]),str(job[1]),'0.00','0.00','15.00']))
+        processes.add(subprocess.Popen(['python','pyplots/time_map.py','p',str(job[0]),str(job[1]),'0.00','0.00','15.00']))
+        if len(processes) >= max_processes:
+            os.wait()
+            processes.difference_update(p for p in processes if p.poll() is not None)
