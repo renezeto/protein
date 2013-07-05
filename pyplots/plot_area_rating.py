f9e3805222779b9ee4c14a6a0e2fb8a0676f02bb
rene <rene@ubuntu.(none)>
2013-07-04 18:57:02 -0700

    wip: plot area rating. goal: plot avg protein density vs area rating. atm: has bugs

--- pyplots/plot_area_rating.py
+++ pyplots/plot_area_rating.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 import file_loader
 
 f_shape = sys.argv[1]
@@ -10,16 +11,16 @@ f_param3 = sys.argv[4]
 f_param4 = sys.argv[5]
 f_param5 = sys.argv[6]
 
-fname = "data/shape-" + f_shape +"area_rating_two-" +f_param1+ "-" +f_param2 + "-" +f_param3+ "-" +f_param4 + "-" +f_param5+ ".dat"
-areaFile = np.loadtxt('fname',dtype=float)
+areaFname = "data/shape-" + f_shape +"/area_rating_two-" +f_param1+ "-" +f_param2 + "-" +f_param3+ "-" +f_param4 + "-" +f_param5+ ".dat"
+areaFile = np.loadtxt(areaFname,dtype=float)
 
-x = np.array([],dtype='float')
-y = np.array([],dtype='float')
-z = np.array([],dtype='float')
-areaRating=[]
+proteinFname = "data/shape-" + f_shape +"/avg_density-" +f_shape+ "-" +f_param1+ "-" +f_param2 + "-" +f_param3+ "-" +f_param4 + "-" +f_param5+ ".out" #change to .dat
+proteinFile = np.loadtxt(proteinFname,dtype=float)
+
+avgProtein = []
+areaRating = []
 for i in range(areaFile.shape[0]):
-    x = append(x,(round(areaFile[i][0]/file_loader.dx)))
-    y = append(y,(round(areaFile[i][1]/file_loader.dx)))
-    z = append(z,(round(areaFile[i][2]/file_loader.dx)))
-    areaRating[y][z] += [areaFile[i][3]]
-areaRating = np.array(areaRating,dtype='float')
+    areaRating += [areaFile[i][3]]
+for i in range(avgProtein.shape[0]):
+    avgProtein += [proteinFile[i][3]]
+print areaRating
