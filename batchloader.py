4bf177b70a9ad71d4d134d04fdafacd0615030b0
Rene Zeto <zetor@bennet.physics.oregonstate.edu>
2013-07-13 09:48:43 -0700

    .

--- batchloader.py
+++ batchloader.py
@@ -36,9 +36,9 @@ if '-sim' in sys.argv:
     if 'randst' in sys.argv:
         for job in batch_randst_simulations:
             processes.add(subprocess.Popen(['srun','./protein_microscopy','randst','%.2f'%job[0],'%.2f'%job[1],'%.2f'%job[2],'%.2f'%job[3],'15.00']))
-        if len(processes) >= max_processes:
-            os.wait()
-            processes.difference_update(p for p in processes if p.poll() is not None)
+            if len(processes) >= max_processes:
+                os.wait()
+                processes.difference_update(p for p in processes if p.poll() is not None)
 
 #plots the recent simulations:
 if '-plot' in sys.argv:
@@ -46,16 +46,16 @@ if '-plot' in sys.argv:
     max_processes = 100
     if 'p' in sys.argv:
         for job in batch_pill_simulations:
-            #processes.add(subprocess.Popen(['python','pyplots/extrema.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
-            #processes.add(subprocess.Popen(['python','pyplots/time_map.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
+            processes.add(subprocess.Popen(['srun','python','pyplots/extrema.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
+            processes.add(subprocess.Popen(['srun','python','pyplots/time_map.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
             processes.add(subprocess.Popen(['srun','python','pyplots/showatp.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
             if len(processes) >= max_processes:
                 os.wait()
                 processes.difference_update(p for p in processes if p.poll() is not None)
     if 'randst' in sys.argv:
         for job in batch_randst_simulations:
-#            processes.add(subprocess.Popen(['python','pyplots/extrema.py','p','%.2f'%job[0],'%.2f'%job[1],'0.00','0.00','15.00']))
-#            processes.add(subprocess.Popen(['python','pyplots/time_map.py','randst','%.2f'%job[0],'%.2f'%job[1],'%.2f'%job[2],'%.2f'%job[3],'15.00']))
+            processes.add(subprocess.Popen(['srun','python','pyplots/extrema.py','randst','%.2f'%job[0],'%.2f'%job[1],'%.2f'%job[2],'%.2f'%job[3],'15.00']))
+            processes.add(subprocess.Popen(['srun','python','pyplots/time_map.py','randst','%.2f'%job[0],'%.2f'%job[1],'%.2f'%job[2],'%.2f'%job[3],'15.00']))
             processes.add(subprocess.Popen(['srun','python','pyplots/showatp.py','randst','%.2f'%job[0],'%.2f'%job[1],'%.2f'%job[2],'%.2f'%job[3],'15.00']))
             if len(processes) >= max_processes:
                 os.wait()
