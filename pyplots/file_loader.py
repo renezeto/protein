4d44ca93133308e768a82744c21e913a11ad4b90
Rene Zeto <zetor@wngr412-pc11.physics.oregonstate.edu>
2013-07-15 09:52:50 -0700

    file loader updated

--- pyplots/file_loader.py
+++ pyplots/file_loader.py
@@ -9,13 +9,20 @@ f_param3 = sys.argv[4]
 f_param4 = sys.argv[5]
 f_param5 = sys.argv[6]
 
-#get_filenames method sloppy but it works.
+dx=.15
 
+#initialize as empty lists incase arguments not present in sys.argv
+hires_str=''
+m_str=''
+
+#change them to print the correct file names if arguments present
 if "-hires" in sys.argv:
     dx=.05
-else:
-    dx=.15
+    hires_str='hires-'
+if "-slice" not in sys.argv:
+    m_str='m-'
 
+#data object, to create in another plot: proteinname=data(protein="proteinname")
 class data(object):
     def __init__(self,protein):
         self.protein = protein
@@ -26,31 +33,19 @@ class data(object):
         self.datasize = [self.datashape[n]*dx for n in range(len(self.datashape))]
         self.axes = [np.arange(0,self.datasize[n],dx) for n in range(len(self.datashape))]
 
+#loads the files for creating the data object.
     @staticmethod
     def get_filenames(protein):
         dat_filenames = []
-        if "-hires" in sys.argv:
-            if "-slice" in sys.argv:
-                for fn in glob.iglob('./data/shape-'+f_shape+'/hires-'+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'):
-                    dat_filenames.append(fn)
-            else:
-                for fn in glob.iglob('./data/shape-'+f_shape+'/hires-m-'+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'):
-                    dat_filenames.append(fn)
-        else:
-            if "-slice" in sys.argv:
-                for fn in glob.iglob('./data/shape-'+f_shape+'/'+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'):
-                    dat_filenames.append(fn)
-            else:
-                for fn in glob.iglob('./data/shape-'+f_shape+'/m-'+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'):
-                    dat_filenames.append(fn)
+        for fn in glob.iglob('./data/shape-'+f_shape+'/'+hires_str+m_str+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'):
+            dat_filenames.append(fn)
         dat_filenames = sorted(dat_filenames)
-        i=0
+        i=0 #pop the first 10% of file names to let things equillibriate a bit
         while i<round(len(dat_filenames)/10):
             dat_filenames.pop(0)
             i+=1
         if (dat_filenames == []):
             print "File loading error: filename list is empty."
-            print './data/shape-'+f_shape+'/m-'+protein+'-'+f_shape+'-'+f_param1+'-'+f_param2+'-'+f_param3+'-'+f_param4+'-'+f_param5+'*.dat'
             exit(1)
         else:
             return dat_filenames
--- pyplots/time_map.py
+++ pyplots/time_map.py
@@ -1,9 +1,12 @@
 from __future__ import division
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import file_loader as load
 import sys
 
+#create protein data objects (see file_loader.py)
 nflE = load.data(protein="nflE")
 nflD = load.data(protein="nflD")
 nATP = load.data(protein="nATP")
@@ -18,12 +21,15 @@ f_param3 = int(10*(float(sys.argv[4])))
 f_param4 = int(10*(float(sys.argv[5])))
 f_param5 = int(10*(float(sys.argv[6])))
 
+#compute time average (removing first 10% happens in file loading)
 def average_location(dataset):
     tsum = np.zeros_like(dataset[0])
     for t in range(nATP.tsteps):
             tsum += dataset[t]
     return tsum/(nATP.tsteps)
 
+print './data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'time-map-compare-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf'
+#create comparison subplots and print them to one file. 
 plt.figure(1)
 plt.subplot(221)
 plt.contourf(nATP.axes[1],nATP.axes[0],average_location(nATP.dataset),500)
@@ -39,20 +45,15 @@ plt.title('nADP', fontsize=20)
 
 plt.subplot(224)
 plt.contourf(Nd.axes[1],Nd.axes[0],average_location(Nd.dataset),500)
-plt.title('Nd', fontsize=20)
-#plt.show(1)
-plt.savefig('./data/shape-'+f_shape+'/plots/time-map-compare-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
+plt.title('Nd', fontsize=20) #load.hires_str and load.m_str are empty strings unless -hires and -slice are in the sys.argv list. see file_loader.py
+plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'time-map-compare-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
 print "time_map plot generated: subplot comparison"
 
+#plot each of the data set time maps individually.
 for p in [nflE, nflD, nATP, nE, nADP, Nd]:
     plt.figure()
     plt.contourf(p.axes[1],p.axes[0],average_location(p.dataset),500)
     plt.axes().set_aspect('equal', 'datalim')
     plt.colorbar()
-    plt.savefig('./data/shape-'+f_shape+'/plots/time-map-'+str(p.protein)+'-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
+    plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'time-map-'+str(p.protein)+'-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
     print "time_map plot generated: " + str(p.protein)
-
-
-# fig.suptitle('test title', fontsize=20)
-# plt.xlabel('xlabel', fontsize=18)
-# plt.ylabel('ylabel', fontsize=16)
