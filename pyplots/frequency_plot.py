4ea9528afecbb25d0797f107521abc05a8620fe9
jeffschulte <jeffschulte83@gmail.com>
2013-07-18 10:11:43 -0700

    modified frequency plotting

--- pyplots/frequency_plot.py
+++ pyplots/frequency_plot.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pylab
 import os
 import sys
 import time
@@ -55,36 +56,64 @@ nATP = load.data(protein="nATP")
 nE = load.data(protein="nE")
 nADP = load.data(protein="nADP")
 Nd = load.data(protein="Nd")
+Nde = load.data(protein="Nde")
 
-for p in [nflE, nflD, nATP, nE, nADP, Nd]:
+proteins = [nflE, nflD, nATP, nE, nADP, Nd, Nde]
+proteins_strings = ["nflE", "nflD", "nATP", "nE", "nADP", "Nd", "Nde"]
+
+for p in proteins:
     for i in p.dataset:
         if (globalmax(i) != "empty"):
             p.pointInfo = (globalmax(i)[0][0], globalmax(i)[0][1])
             break
     print p.pointInfo
 
-z = 2 + round((float(f_param1)-.15)/dx)
-y = 2 + round((float(f_param2)-.15)/dx)
-print z*dx
-print y*dx
-print "Hello"
-print z
-print y
-pointB = ()
+mid_z = 2 + round((float(f_param1)-.15)/dx)
+mid_y = 2 + round((float(f_param2)-.15)/dx)
 
-for p in [nflE, nflD, nATP, nE, nADP, Nd]:
+
+for p in proteins:
     p.densityInfo = []
     p.densityInfoTwo = []
-    print p.pointInfo[0]
-    print p.pointInfo[1]
     for file in p.dataset:
         p.densityInfo += [ file[p.pointInfo[0]][p.pointInfo[1]] ]
-        p.densityInfoTwo += [ file[y][z] ]
+        p.densityInfoTwo += [ file[mid_y][mid_z] ]
     time = np.arange(0,len(p.densityInfo)/2,.5)
     plt.figure()
     plt.plot(time,p.densityInfo)
     plt.plot(time,p.densityInfoTwo)
     plt.title('Max point = '+str(p.pointInfo[0]*dx)+','+str(p.pointInfo[0]*dx)
-              +'   Middle point = '+str(y*dx)+','+str(z*dx))
+              +'   Middle point = '+str(mid_y*dx)+','+str(mid_z*dx))
     plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_plot'+str(p.protein)+'-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
 
+
+norm = False
+if "-norm" in sys.argv:
+    norm = True
+    print "We are normalizing the plots so the highest density for each protein will be one"
+
+plt.figure()
+for p in proteins:
+    pointInfoWall = Nd.pointInfo
+    if p!=Nd and p!=Nde:
+        pointInfoWall = (int(Nd.pointInfo[0]),int(Nd.pointInfo[1]))
+    p.densityInfo = []
+    #p.densityInfoTwo = []
+    for file in p.dataset:
+        p.densityInfo += [ file[pointInfoWall[0]][pointInfoWall[1]] ]
+    if norm:
+        for i in np.arange(len(p.densityInfo)):
+            p.densityInfo[i]=p.densityInfo[i]/max(p.densityInfo)
+    time = np.arange(0,len(p.densityInfo)/2,.5)
+    plt.plot(time,p.densityInfo)
+    #plt.legend(proteins_strings,loc='best')
+    #plt.plot(time,p.densityInfoTwo)
+if norm:
+    plt.ylim(0,1.5)
+plt.title('Max point = '+str(pointInfoWall[0]*dx)+','+str(pointInfoWall[0]*dx))
+plt.legend(proteins_strings,loc='best')
+#pylab.legend(loc='best')
+if norm:
+    plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_plot_all_norm-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
+else:
+        plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_plot_all-'+f_shape+'-'+str(f_param1)+'-'+str(f_param2)+'-'+str(f_param3)+'-'+str(f_param4)+'-'+str(f_param5)+'.pdf')
