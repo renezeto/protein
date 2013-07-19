e424be2ed7863ddd754354b70c5e5111b3347cee
jeffschulte <jeffschulte83@gmail.com>
2013-07-19 14:45:26 -0700

    added to frequency_plot.py the next two plots that Roundy suggested

--- pyplots/frequency_plot.py
+++ pyplots/frequency_plot.py
@@ -57,8 +57,8 @@ par4 = str(int(10*(float(f_param4))))
 par5 = str(int(10*(float(f_param5))))
 
 
-nflE = load.data(protein="nflE")
-nflD = load.data(protein="nflD")
+NflE = load.data(protein="nflE")
+NflD = load.data(protein="nflD")
 nATP = load.data(protein="nATP")
 nE = load.data(protein="nE")
 nADP = load.data(protein="nADP")
@@ -76,10 +76,10 @@ if "-norm" in sys.argv:
     print "We are normalizing the plots so the highest density for each protein will be one"
 
 
-proteins = [nflD, nATP, nE, nADP, Nd, Nde]#normally have nflD and nflE too
-proteins_strings = ["nATP", "nE", "nADP", "Nd", "Nde"] #normally "nflD" and "nflE"
+proteins = [NflD, nATP, nE, nADP, Nd, Nde]#normally have nflD and nflE too
+proteins_strings = ["NflD","nATP", "nE", "nADP", "Nd", "Nde"] #normally "NflD" and "NflE"
 if mid:
-    proteins_strings = ["nflD", "middle-nflD", "nATP", "middle-nATP","nE", "middle-nE","nADP", "middle-nADP"] #normally "nflD" and "nflE"
+    proteins_strings = ["NflD", "middle-NflD", "nATP", "middle-nATP","nE", "middle-nE","nADP", "middle-nADP"] #normally "NflD" and "NflE"
 
 
 
@@ -141,3 +141,105 @@ elif mid:
     plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_plot_all_middle-'+f_shape+'-'+par1+'-'+par2+'-'+par3+'-'+par4+'-'+par5+'.pdf')
 else:
     plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_plot_all-'+f_shape+'-'+par1+'-'+par2+'-'+par3+'-'+par4+'-'+par5+'.pdf')
+
+right_cutoff_index = 0
+left_cutoff_index = 0
+if f_shape == "p":
+    right_cutoff_index = int(2.0*float(f_param1)/(dx*3.0) + 2)
+    left_cutoff_index = int(float(f_param1)/(dx*3.0) + 2)
+if f_shape == "randst":
+    right_cutoff_index = int(2.0*float(f_param2)/(dx*3.0) + 2)
+    left_cutoff_index = int(float(f_param2)/(dx*3.0) + 2)
+
+
+max_z_index = 0.0
+max_y_index = 0.0
+if f_shape == "p":
+    max_z_index = int(float(f_param1)/dx)+4
+    max_y_index = int(float(f_param2)/dx)+4
+if f_shape == "randst":
+    max_z_index = int(float(f_param2)/dx)+4
+    max_y_index = int(float(f_param2)/dx)+4
+
+total =0.0
+for file in NflD.dataset:
+    for i in range(max_z_index):
+        for j in range(max_y_index):
+            total += file[i][j]
+    print total
+    total = 0.0
+
+
+
+split_densities_strings = ["right density", "center density", "left density"]
+
+for p in proteins:
+    right_densities = [0]
+    center_densities = [0]
+    left_densities = [0]
+    for file in p.dataset:
+        right_density = 0.0
+        center_density = 0.0
+        left_density = 0.0
+        for i in range(max_z_index):
+            for j in range(max_y_index):
+                if i >= right_cutoff_index:
+                    right_density += file[j][i]
+                elif i <= left_cutoff_index:
+                    left_density += file[j][i]
+                else:
+                    center_density += file[j][i]
+        right_densities += [right_density]
+        center_densities += [center_density]
+        left_densities += [left_density]
+    for i in range(len(center_densities)):
+        center_densities[i] = right_densities[i] + center_densities[i]
+    for i in range(len(left_densities)):
+        left_densities[i] = center_densities[i] + left_densities[i]
+    time = np.arange(0,len(right_densities)/2,.5)
+    plt.figure()
+    plt.plot(time,right_densities)
+    plt.plot(time,center_densities)
+    plt.plot(time,left_densities)
+    plt.legend(split_densities_strings,loc='best')
+    protein_name = ""
+    for i in range(len(proteins)):
+        if p == proteins[i]:
+            protein_name = proteins_strings[i]
+    plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_split_pill-'+protein_name+'-'+f_shape+'-'+par1+'-'+par2+'-'+par3+'-'+par4+'-'+par5+'.pdf')
+
+plt.figure()
+
+
+print "HERE"
+#print len(summing_densities)
+
+for p in proteins:
+    if (p == nATP or p == nADP or p == nE):
+        for file in p.dataset:
+            file[i][j] = dx*dx*dx*file[i][j]
+
+
+summing_densities = [0]*((max_y_index-1)*(max_z_index-1))
+print len(summing_densities)
+for p in proteins:
+    p.total_densities = [0]
+    for file in p.dataset:
+        total_density = 0.0
+        for i in range(max_z_index):
+            for j in range(max_y_index):
+                if (p == nATP or p == nADP or p == nE):
+                    total_density += dx*dx*dx*file[j][i]
+                else:
+                    total_density +=file[j][i]
+        p.total_densities += [total_density]
+    print len(p.total_densities)
+    for i in range(len(p.total_densities)):
+        p.total_densities[i] = summing_densities[i] + p.total_densities[i]
+        summing_densities[i] += p.total_densities[i]
+    time = np.arange(0,len(NflD.total_densities)/2,.5)
+    plt.plot(time,p.total_densities)
+
+plt.savefig('./data/shape-'+f_shape+'/plots/'+load.hires_str+load.m_str+'frequency_total_pill-'+protein_name+'-'+f_shape+'-'+par1+'-'+par2+'-'+par3+'-'+par4+'-'+par5+'.pdf')
+
+  
