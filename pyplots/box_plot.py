79e85131fb3064af7053437aa83b0d5058eb5725
Rene <rene.zeto@gmail.com>
2013-08-12 19:32:17 -0700

    wip but the two existing functions work.

--- pyplots/box_plot.py
+++ pyplots/box_plot.py
@@ -7,39 +7,54 @@ import sys
 import pylab
 import file_loader_dump as load
 
-## WIP!! 
+## WIP!!
 
-#[[j+k for (j,k) in zip(plot_lines[i-1], p.lines[i])]]
+#reads a special kind of data file printed by protein_microscopy.cpp
+#format is:
+# --------
+# proteinType1 boxName1 n(t=0) n(t=1) n(t=2) ...
+# proteinType1 boxName2 n(t=0) n(t=1) n(t=2) ...
+# proteinType1 boxName3 n(t=0) n(t=1) n(t=2) ...
+#
+# proteinType2 boxName1 n(t=0) n(t=1) n(t=2) ...
+# proteinType2 boxName2 n(t=0) n(t=1) n(t=2) ...
+# proteinType2 boxName3 n(t=0) n(t=1) n(t=2) ...
+# --------
+#where n(t) is the number of proteins of one type at time t
 
-#this is the ordering of proteins in the box plot list
-# sprintf(proteinList[0]->name,"nATP");
-# sprintf(proteinList[1]->name,"nADP");
-# sprintf(proteinList[2]->name,"nE");
-# sprintf(proteinList[3]->name,"ND");
-# sprintf(proteinList[4]->name,"NDE");
-#starting with nATP, it's 3 lines (one for each box), break, then nADP, etc
-#each column (moving right) represents a time step
+#opens the file and grabs a particular line matching proteinType and boxName. returns list of protein counts at each time.
+def returnData(proteinType,boxName):
 
-#box_data = np.loadtxt("../data/shape-%s/box-plot--%s-%s-%s-%s-%s.dat"%(load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
+    #open the data file, grab the line with the correct protein type and box partition, load it as a [string] (so we can use list comprehensions)
+    with open("./data/shape-%s/box-plot--%s-%s-%s-%s-%s.dat"%(load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5),"r") as boxData:
+        proteinsOverTime = [line for line in boxData if (proteinType in line) and (boxName in line)]
 
-def returnData(protein,boxName):
-    with open("../data/shape-%s/box-plot--%s-%s-%s-%s-%s.dat"%(load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5),"r") as boxData:
-        proteinsOverTime = [line for line in f if (protein in line) and (boxName in line)]
-    return proteinsOvertime
+    #format the string so that it is a list of numbers (split on tab, pop off keywords and newlines, convert str -> float)
+    proteinsOverTime = proteinsOverTime[0].split('\t')
+    proteinsOverTime = proteinsOverTime[2:-1]
+    proteinsOverTime = [float(i) for i in proteinsOverTime]
 
+    return proteinsOverTime
+
+#takes input format: ["proteinType1-boxNum1","proteinType1-boxnum2",proteinType2-boxnum1"...]. will return a list of lists
+#in the stacking order specified by the input (first entry is at the bottom).
 def stackData(plotList):
+
+    #parse the input
     tempList = []
     for proteinData in plotList:
         splitString = proteinData.split('-')
         (protein, boxName) = (splitString[0], splitString[1])
-        tempList += [[returnData(protein, boxName)]]
+        tempList += [returnData(protein, boxName)]
+
+    #"stack" the lists
     stackedPlotList = [tempList[0]]
-    for i in range(1,len(stackedPlotList)):
+    for i in range(1,len(tempList)):
         stackedPlotList += [[j+k for (j,k) in zip(tempList[i-1],tempList[i])]]
+
     return stackedPlotList
 
 def main():
-    #enter command line arguments and it will automatically generate the right plot
     return 0
 
 if __name__ == '__main__':
