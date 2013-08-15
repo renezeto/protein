ae730e5f120eea232436c6906720eafd85cd35e0
Rene <rene.zeto@gmail.com>
2013-08-14 18:46:47 -0700

    time map also works now

--- pyplots/time_map.py
+++ pyplots/time_map.py
@@ -3,12 +3,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import file_loader as load
+import file_loader_dump as load
 import sys
-##
-
 
 for protein in load.proteinList:
     plt.figure()
-    contourf(np.loadtxt("./data/shape-%s/time-map-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5)))
+    plt.contourf(np.loadtxt("./data/shape-%s/time-map-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5)))
     plt.savefig("./data/shape-%s/plots/time-map-%s-%s-%s-%s-%s-%s.pdf"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
