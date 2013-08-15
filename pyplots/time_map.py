7e2c779b6bd45937192939bbed688842249647ab
Rene <rene.zeto@gmail.com>
2013-08-14 20:09:36 -0700

    updated time map

--- pyplots/time_map.py
+++ pyplots/time_map.py
@@ -6,7 +6,16 @@ import numpy as np
 import file_loader_dump as load
 import sys
 
+# WIP
+
 for protein in load.proteinList:
+    file = np.loadtxt("./data/shape-%s/time-map-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
     plt.figure()
-    plt.contourf(np.loadtxt("./data/shape-%s/time-map-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5)))
+    plt.pcolor(file)
+    plt.xlim(0,file.shape[1])
+    plt.ylim(0,file.shape[0])
+    plt.xlabel("Z grid position")
+    plt.ylabel("Y grid position")
+    plt.title("Time averaged view of %s"%(protein))
+    plt.colorbar()
     plt.savefig("./data/shape-%s/plots/time-map-%s-%s-%s-%s-%s-%s.pdf"%(load.f_shape,protein,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
