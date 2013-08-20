e9cb6ec50a829aae9094caa5c4c1a57250e4ec41
Rene <rene.zeto@gmail.com>
2013-08-19 19:44:35 -0700

    updated to catch special sim flags

--- pyplots/time_map.py
+++ pyplots/time_map.py
@@ -9,7 +9,7 @@ import sys
 # WIP
 
 for protein in load.proteinList:
-    file = np.loadtxt("./data/shape-%s/time-map-%s-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
+    file = np.loadtxt("./data/shape-%s/%s%stime-map-%s-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,load.hires_str,load.slice_str,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
     plt.figure()
     plt.pcolor(file)
     plt.xlim(0,file.shape[1])
@@ -18,4 +18,4 @@ for protein in load.proteinList:
     plt.ylabel("Y grid position")
     plt.title("Time averaged view of %s"%(protein))
     plt.colorbar()
-    plt.savefig("./data/shape-%s/plots/time-map-%s-%s-%s-%s-%s-%s-%s.pdf"%(load.f_shape,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
+    plt.savefig("./data/shape-%s/%s%stime-map-%s-%s-%s-%s-%s-%s-%s.pdf"%(load.f_shape,load.hires_str,load.slice_str,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
