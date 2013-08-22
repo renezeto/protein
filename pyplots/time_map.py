f81a493676d642a3b2ff8e0b1b616ce31e40f4a9
renezeto <rene.zeto@gmail.com>
2013-08-22 13:20:55 -0700

    .

--- pyplots/time_map.py
+++ pyplots/time_map.py
@@ -9,7 +9,7 @@ import sys
 # WIP
 
 for protein in load.proteinList:
-    file = np.loadtxt("./data/shape-%s/%s%stime-map-%s-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,load.hires_str,load.slice_str,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
+    file = np.loadtxt("./data/shape-%s/%s%s%stime-map-%s-%s-%s-%s-%s-%s-%s.dat"%(load.f_shape,load.debug_str,load.hires_str,load.slice_str,protein,load.f_shape,load.f_param1,load.f_param2,load.f_param3,load.f_param4,load.f_param5))
     plt.figure()
     plt.pcolor(file)
     plt.xlim(0,file.shape[1])
