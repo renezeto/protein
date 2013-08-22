3880db7e8f127af013e6c1330c958fc04119b543
renezeto <rene.zeto@gmail.com>
2013-08-22 11:35:18 -0700

    test

--- pyplots/file_loader.py
+++ pyplots/file_loader.py
@@ -45,7 +45,7 @@ class data(object):
     @staticmethod
     def get_filenames(protein):
         dat_filenames = []
-        for fn in glob.iglob("./data/shape-"+f_shape+"/"debug_str+hires_str+slice_str+protein+"-"+f_shape+"-"+f_param1+"-"+f_param2+"-"+f_param3+"-"+f_param4+"-"+f_param5+"*.dat"):
+        for fn in glob.iglob("./data/shape-%s/%s%s%s%s-%s-%s-%s-%s-%s-%s-*.dat"%(f_shape,debug_str,hires_str,slice_str,protein,f_shape,f_param1,f_param2,f_param3,f_param4,f_param5)):
             dat_filenames.append(fn)
         dat_filenames = sorted(dat_filenames)
         i=0 #pop the first 10% of file names to let things equillibriate a bit
@@ -54,7 +54,7 @@ class data(object):
             i+=1
         if (dat_filenames == []):
             print "File loading error: filename list is empty."
-            print "./data/shape-"+f_shape+"/"debug_str+hires_str+slice_str+protein+"-"+f_shape+"-"+f_param1+"-"+f_param2+"-"+f_param3+"-"+f_param4+"-"+f_param5+"*.dat"
+            print "./data/shape-%s/%s%s%s%s-%s-%s-%s-%s-%s-%s-*.dat"%(f_shape,debug_str,hires_str,slice_str,protein,f_shape,f_param1,f_param2,f_param3,f_param4,f_param5)
             exit(1)
         else:
             return dat_filenames
@@ -64,3 +64,5 @@ def print_string(plot_name,p):
     arg = [str(int(100*(float(i)))) for i in sys.argv[2:7]]
     filename = "./data/shape-%s/plots/%s%s%s%s-%s-%s-%s-%s-%s-%s-%s.pdf"%(f_shape, debug_str, hires_str, slice_str, plot_name, p, f_shape, arg[0], arg[1], arg[2], arg[3], arg[4])
     return filename
+
+#"./data/shape-"+f_shape+"/"debug_str+hires_str+slice_str+protein+"-"+f_shape+"-"+f_param1+"-"+f_param2+"-"+f_param3+"-"+f_param4+"-"+f_param5+"*.dat"
