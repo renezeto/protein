a53e25b998c7809e1c59398ae02d653935f72198
jeffschulte <jeffschulte83@gmail.com>
2013-08-31 17:00:07 -0700

    altered file loader and box plot so picks up extra 2 arguments from command line that give the start and finish of each plot. changed jobs to give an example of what I have in mind, although the make papers doesnt want to incorporate changes into paper Tested everything on node23.

--- pyplots/file_loader.py
+++ pyplots/file_loader.py
@@ -35,9 +35,9 @@ if "-debug" in sys.argv:
 if "-paper" in sys.argv:
     expected_arg_number += 1
 
-#if len(sys.argv)>expected_arg_number:
-f_param6 = sys.argv[7]
-f_param7 = sys.argv[8]
+if len(sys.argv)>expected_arg_number:
+    f_param6 = sys.argv[7]
+    f_param7 = sys.argv[8]
 
 filename_tuple = (f_shape,hires_str,slice_str,f_shape,f_param1,f_param2,f_param3,f_param4,f_param5)
 
