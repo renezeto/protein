3950c7c529bf78a7329f7b2b5fb1ac1903a13b1d
David Roundy <roundyd@physics.oregonstate.edu>
2013-11-15 14:59:12 -0800

    updates to paper and pretty figure Tested everything on collins.

--- pyplots/image_plot.py
+++ pyplots/image_plot.py
@@ -33,7 +33,7 @@ proteinList = ["nADP","nATP","ND","NDE","nE"]
 proteinLabels = ["MinD:ADP (ctyo)",
                  "MinD:ATP (cyto)",
                  "MinD:ATP (mem)",
-                 "MinE:MinD:ATP",
+                 "MinE:MinD:ATP (mem)",
                  "MinE (cyto)"]
 
 proteins = [0]*len(proteinList)
@@ -41,12 +41,13 @@ proteins = [0]*len(proteinList)
 for i in range(len(proteinList)):
     proteins[i] = load.data(proteinList[i],start_time,end_time)
 
-plt.figure(figsize=(10,4))
+plt.figure(figsize=(9,3.5))
 numtimes = int(end_time/dump_time_step)- int(start_time/dump_time_step)
 numproteins = len(proteins)
+skip_times = 2 # only plot every skip_times of the snapshots
 dZ = proteins[0].datashape[1]*1.05
-dY = proteins[0].datashape[0]*1.1
-kvals = range(int(end_time/dump_time_step)-int(start_time/dump_time_step))
+dY = proteins[0].datashape[0]*1.1/skip_times
+kvals = range(0, int(end_time/dump_time_step)-int(start_time/dump_time_step), skip_times)
 
 #axes = plt.subplot(1, 1, 1, axisbg='black')
 
@@ -55,10 +56,11 @@ for i in range(len(proteins)):
         maxval = timemax(proteins[i].dataset)/4
     else:
         maxval = timemax(proteins[i].dataset)
-    minval = timemin(proteins[i].dataset[int(start_time/dump_time_step):])
-    nlevels = 10
-    mylevels = np.linspace(minval,(1+1.0/nlevels)*maxval,nlevels)
-    Z, Y = np.meshgrid(np.arange(0,proteins[i].datashape[1],1), np.arange(0,proteins[i].datashape[0],1))
+    #minval = timemin(proteins[i].dataset[int(start_time/dump_time_step):])
+    nlevels = 20
+    mylevels = np.linspace(0,(1+1.0/nlevels)*maxval,nlevels)
+    Z, Y = np.meshgrid(np.arange(0,proteins[i].dataset[0].shape[1],1),
+                       np.arange(0,proteins[i].dataset[0].shape[0],1))
 #generate a sequence of .png's for each file (printed time step). these will be used to create a gif.
     for k in kvals:
         page = proteins[i].dataset[k]
@@ -66,9 +68,22 @@ for i in range(len(proteins)):
         plt.contourf(Y+k*dY, Z+i*dZ, page, cmap=plt.cm.hot_r, levels=mylevels)
 plt.axes().get_yaxis().set_ticks([(i+0.5)*dZ for i in range(len(proteinList))])
 plt.axes().get_yaxis().set_ticklabels(proteinLabels)
-plt.axes().get_xaxis().set_ticks([(0.5+k)*dY for k in kvals[::4]])
-plt.axes().get_xaxis().set_ticklabels([int(k*dump_time_step) for k in kvals[::4]])
+plt.axes().get_xaxis().set_ticks([(0.5+k)*dY for k in kvals[::5*skip_times]])
+plt.axes().get_xaxis().set_ticklabels([int(k*dump_time_step) for k in kvals[::5*skip_times]])
 plt.axes().set_aspect('equal')
+plt.axes().get_yaxis().set_ticks_position('none')
+plt.axes().get_xaxis().set_ticks_position('none')
 plt.xlabel('time (s)')
+
+# I got the colorbar bit here from
+# http://matplotlib.org/users/tight_layout_guide.html, which explains
+# the difficulties with tight_layout() and colorbar positioning.
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+divider = make_axes_locatable(plt.gca())
+cax = divider.append_axes("right", "3%", pad="1%")
+cbar = plt.colorbar(cax=cax,ticks=[0,maxval])
+cbar.ax.set_yticklabels(['0', 'max'])
+
+#plt.colorbar(ticks=[])
 plt.tight_layout()
 plt.savefig(load.print_string("image-plot",""))
